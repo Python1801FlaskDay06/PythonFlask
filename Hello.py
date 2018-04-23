ccf424e21033b8e1ca3d379849922fa2e8cca4f1
MD1494 <961065897@qq.com>
2018-04-23 15:11:10 +0800

    额

--- Hello.py
+++ Hello.py
@@ -1,19 +1,2 @@
-
-print("你真是一个小天才!")
-
-print("好开心啊，今天又能吃成长快乐啦！")
-
-print("听说你百米只需12秒")
-
-hello = 'python1801'
-hallo = 'python1801'
-
-print(hallo)
-
-print(hello)
-
-print("或许终于能提交了")
-
-
-
+print("都删了，这日子不过了")
 
