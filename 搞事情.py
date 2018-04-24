71c716e61d117a96e2abccf16953383437798490
Rock1204 <rongjiawei@1000phone.com>
2018-04-24 10:54:07 +0800

    暴力破解

--- 搞事情.py
+++ 搞事情.py
@@ -6,3 +6,15 @@ print(a)
 
 print("放假倒计时，周日放假")
 
+print("这有好玩的")
+
+print("给你们推荐一个包， nmap 端口扫描")
+
+print("爆破")
+
+print("穷举，输入所有的可能结果")
+
+print("22 端口，ssh， 3306 端口， mysql")
+
+print("pymysql. client， host , port, user , password")
+
